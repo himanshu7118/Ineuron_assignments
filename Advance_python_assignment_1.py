11eb31d332ac77c1e2fe43f0ee2d9e6188736b06
himanshu7118 <75970378+himanshu7118@users.noreply.github.com>
2023-03-07 20:27:46 +0530

    Create Advance_python_assignment_1.py

---
+++ Advance_python_assignment_1.py
@@ -0,0 +1,37 @@
+Q1. What is the purpose of Python's OOP?
+Ans: Object-oriented programming is a programming paradigm that structures programs so that properties and behaviors are bundled into individual objects.
+
+
+
+Q2. Where does an inheritance search look for an attribute?
+Ans: An inheritance search looks for an attribute first in the instance object, then in the class the instance was created from, then in all higher superclasses, progressing from left to right by default. The search stops at the first place the attribute is found.
+
+
+
+Q3. How do you distinguish between a class object and an instance object?
+Ans: A class is a type of blueprint that you can use to make objects. A concrete 'thing' you constructed using a certain class is an object called an instance of a class.
+
+
+
+Q4. What makes the first argument in a class’s method function special?
+Ans:  self makes the first argument in a class’s method function special.
+
+
+
+Q5. What is the purpose of the __init__ method?
+Ans: The __init__ method lets the class initialize the object's attributes and serves no other purpose. It is only used within classes.
+
+
+
+Q6. What is the process for creating a class instance?
+Ans: To create instances of a class, you call the class using class name and pass in whatever arguments its __init__ method accepts.
+
+
+
+Q7. What is the process for creating a class?
+Ans: Enter the class name. To enter a short description, grade level, or class time, tap Section and enter the details. To enter the location for the class, tap Room and enter the details.To add a subject, tap Subject and enter a name.
+
+
+
+Q8. How would you define the superclasses of a class?
+Ans: A class that is derived from another class is called a subclass. The class from which the subclass is derived is called a superclass.
